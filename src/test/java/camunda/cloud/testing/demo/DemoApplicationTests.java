0bc697f6dabce2bb131d74b6b40a39712e79acd6
Thomas <thomas.heinrichs@camunda.com>
2021-12-22 10:23:20 +0100

    Update to 1.3.0-alpha3

--- src/test/java/camunda/cloud/testing/demo/DemoApplicationTests.java
+++ src/test/java/camunda/cloud/testing/demo/DemoApplicationTests.java
@@ -1,37 +1,36 @@
 package camunda.cloud.testing.demo;
 
-import io.camunda.testing.assertions.BpmnAssert;
-import io.camunda.testing.assertions.DeploymentAssert;
-import io.camunda.testing.assertions.JobAssert;
-import io.camunda.testing.assertions.ProcessInstanceAssert;
-import io.camunda.testing.extensions.ZeebeAssertions;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ActivateJobsResponse;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.response.DeploymentEvent;
 import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
-import org.camunda.community.eze.RecordStreamSource;
-import org.camunda.community.eze.ZeebeEngine;
-import org.camunda.community.eze.ZeebeEngineClock;
-import org.junit.jupiter.api.Test;
+import io.camunda.zeebe.process.test.assertions.BpmnAssert;
+import io.camunda.zeebe.process.test.assertions.DeploymentAssert;
+import io.camunda.zeebe.process.test.assertions.JobAssert;
+import io.camunda.zeebe.process.test.assertions.ProcessInstanceAssert;
+import io.camunda.zeebe.process.test.extensions.ZeebeProcessTest;
+import io.camunda.zeebe.process.test.testengine.InMemoryEngine;
+import io.camunda.zeebe.process.test.testengine.RecordStreamSource;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
-@ZeebeAssertions
-class DemoApplicationTests {
+@ZeebeProcessTest
+public class DemoApplicationTests {
+	private InMemoryEngine engine;
 	private ZeebeClient client;
-	private ZeebeEngine engine;
-	private ZeebeEngineClock clock;
 	private RecordStreamSource recordStreamSource;
 
-	void initDeployment(){
+	private void initDeployment(){
 		client.newDeployCommand()
 				.addResourceFromClasspath("test-process.bpmn")
 				.send()
 				.join();
 	}
 
-	ProcessInstanceAssert initProcessInstanceStart(){
+	private ProcessInstanceAssert initProcessInstanceStart(){
 		ProcessInstanceEvent event = client.newCreateInstanceCommand()
 				.bpmnProcessId("TestProcess")
 				.latestVersion()
@@ -41,7 +40,7 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	void testDeployment() {
+	public void testDeployment() {
 		//When
 		DeploymentEvent event = client.newDeployCommand()
 				.addResourceFromClasspath("test-process.bpmn")
@@ -53,7 +52,7 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	void testProcessInstanceStart(){
+	public void testProcessInstanceStart(){
 		//Given
 		initDeployment();
 
@@ -70,7 +69,7 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	void testJobAssertion() throws InterruptedException {
+	public void testJobAssertion() throws InterruptedException {
 		//Given
 		initDeployment();
 		initProcessInstanceStart();
@@ -90,7 +89,7 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	void testCompletionOfInstance() throws InterruptedException {
+	public void testCompletionOfInstance() throws InterruptedException {
 		//Given
 		initDeployment();
 		ProcessInstanceAssert instanceAssert = initProcessInstanceStart();
